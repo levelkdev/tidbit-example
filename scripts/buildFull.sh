42fcfadbd5083e6c577107eacb6ad4a41bc26278
Chris Whinfrey <cwhinfrey@gmail.com>
2018-08-09 18:50:00 -0700

    Add buildFull.sh script

---
+++ scripts/buildFull.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+ORACLE=ExampleOracle.sol
+
+OUTPUT=full
+
+# Create directory if it does not exist
+if [ ! -d $PWD/full ]; then
+  mkdir -p $PWD/full;
+fi
+
+truffle-flattener contracts/$ORACLE > $OUTPUT/$ORACLE
