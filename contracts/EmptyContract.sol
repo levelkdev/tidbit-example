4609e12f908d4951e709353148d8589b38c55254
Chris Whinfrey <cwhinfrey@gmail.com>
2018-10-26 11:23:44 -0700

    ETHPriceBet deploy script

---
+++ contracts/EmptyContract.sol
@@ -0,0 +1,8 @@
+pragma solidity ^0.4.24;
+
+import "tidbit-eth/contracts/Oracles/BasicOracle.sol";
+import "tidbit-eth/contracts/Oracles/MedianOracle.sol";
+
+contract EmptyContract {
+  
+}
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,8 +1,13 @@
-/* globals artifacts */
+/* globals artifacts web3 */
 
-const ETHPriceBet = artifacts.require('ETHPriceBet')
-const BasicOracle = artifacts.require('tidbit-eth/BasicOracle')
-const MedianOracle = artifacts.require('tidbit-eth/MedianOracle')
+global.artifacts = artifacts;
+global.web3 = web3;
+
+const { Contracts, SimpleProject  } = require('zos-lib')
+
+const ETHPriceBet = Contracts.getFromLocal('ETHPriceBet')
+const BasicOracle = Contracts.getFromLocal('BasicOracle')
+const MedianOracle = Contracts.getFromLocal('MedianOracle')
 
 const overAccount = web3.eth.accounts[1]
 const underAccount = web3.eth.accounts[2]
@@ -13,14 +18,28 @@ const oracleDataSource3 = web3.eth.accounts[5]
 
 module.exports = async (callback) => {
 
-  // // Deploy base level oracles
-  // const oracle1 = BasicOracle.new(oracleDataSource1)
-  // const oracle2 = BasicOracle.new(oracleDataSource2)
-  // const oracle3 = BasicOracle.new(oracleDataSource3)
-  // 
-  // // Deploy MedianOracle
-  // const medianOracle = MedianOracle.new([oracle1, oracle2, oracle3])
-  // 
-  // // Deploy betting contract
-  // const ethPriceBet = ETHPriceBet.new(250, overAccount, underAccount, medianOracle.address)
+  console.log('Creating an ETHPriceBet project')
+  const project = new SimpleProject('ETHPriceBet', { from: web3.eth.accounts[0] })
+
+  console.log('Creating 3 BasicOracles contracts')
+  const oracle1 = await project.createProxy(BasicOracle, { initArgs: [oracleDataSource1] })
+  const oracle2 = await project.createProxy(BasicOracle, { initArgs: [oracleDataSource2] })
+  const oracle3 = await project.createProxy(BasicOracle, { initArgs: [oracleDataSource3] })
+  console.log('Oracle 1: ' + oracle1.address + ' dataSource: ' + oracleDataSource1)
+  console.log('Oracle 2: ' + oracle2.address + ' dataSource: ' + oracleDataSource2)
+  console.log('Oracle 3: ' + oracle3.address + ' dataSource: ' + oracleDataSource3)
+
+  console.log()
+
+  console.log('Creating MedianOracle contract')
+  const medianOracle = await project.createProxy(MedianOracle, { initArgs: [[oracle1.address, oracle2.address, oracle3.address]] })
+  console.log('MedianOracle: ' + medianOracle.address)
+
+  console.log()
+
+  console.log('Creating ETHPriceBet contract')
+  const ethPriceBet = await project.createProxy(ETHPriceBet, { initArgs: [250, overAccount, underAccount, medianOracle.address] })
+  console.log('ETHPriceBet: ' + ethPriceBet.address)
+
+  console.log()
 }
